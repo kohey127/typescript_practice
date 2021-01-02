d8d6f67620a44c22285a164073a41be02601fa6c
kohey127 <koooochoki@gmail.com>
2021-01-02 13:58:00 +0900

    Stateの使い方

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Child from './components/Child'
+import { toEditorSettings } from 'typescript';
 
-function App() {
+const App: React.FC = () => {
+    // const [ count, setCount ] = useState<number>(0)
+
+    // const handleIncrement = () => {
+    //     setCount(count + 1)
+    // }
+
+    // const handleDecrement = () => {
+    //     setCount(count - 1)
+    // }
+
+    const [ message, setMessage ] = useState<string>('')
+
+    const handleMessage = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setMessage(e.target.value)
+    }
+    
     return (
         <div className="App">
-            <Child message="子コンポーネントに渡す">
-                <h1>チルドレンで表示</h1>
-            </Child>
+            <p className="title" >{ message }</p>
+            <input type="text" value={message} onChange={handleMessage} />
         </div>
     );
 }
