9b02fd1bab5aaa7a003d81e01c5c1264a627cd4c
kohey127 <koooochoki@gmail.com>
2021-01-03 04:16:27 +0900

    ToDoアプリを作ろう

---
+++ react-todo/src/components/TaskItem.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Task } from '../Types';
+
+type Props = {
+    task: Task
+    handleDone: (task: Task) => void
+    handleDelete: (task: Task) => void
+}
+
+const TaskItem: React.FC<Props> = ({ task, handleDone, handleDelete }) => {
+    return (
+        <li className={task.done ? 'done' : ''}>
+            <label>
+                <input
+                    type="checkbox"
+                    className="checkbox-input"
+                    onClick={() => handleDone(task) }
+                />
+                <span className="checkbox-label">{ task.title }</span>
+            </label>
+            <button
+                className="btn is-delete"
+                onClick={()=>{ handleDelete(task)}}
+            >削除</button>
+        </li>
+    )
+}
+export default TaskItem;
